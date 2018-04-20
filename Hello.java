542c18143803f8ea29f4fedca15c28a16e31e8e2
Kekoa Santana <ksantan1@my.hpu.edu>
2018-04-20 11:12:35 -1000

    Adding file that prints hello

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package hello;
+import java.util.Scanner;
+/** ***********************
+* @author Kekoa Santana
+* @Date: _put_date_here_
+* Assignment: _put_name_of_assignment_here_
+*
+* PUT_SHORT_DESCRIPTIION_OF_ALGORITHM_HERE
+*
+* ***********************/
+public class Hello {
+ public static void main(String[] args) {
+System.out.println("Hello");
+
+
+
+ } // end of main()
+} // end of Hello class

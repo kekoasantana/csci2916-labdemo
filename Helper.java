60987162748c9f3b64ee66af49d37532582dd7b9
Kekoa Santana <ksantan1@my.hpu.edu>
2018-04-20 11:24:35 -1000

    added program to teach about git log

---
+++ Helper.java
@@ -0,0 +1,18 @@
+package helper;
+import java.util.Scanner;
+/** ***********************
+* @author Kekoa Santana
+* @Date: _put_date_here_
+* Assignment: _put_name_of_assignment_here_
+*
+* PUT_SHORT_DESCRIPTIION_OF_ALGORITHM_HERE
+*
+* ***********************/
+public class Helper {
+ public static void main(String[] args) {
+
+    System.out.println("Use git log to see history");
+
+
+ } // end of main()
+} // end of Helper class
